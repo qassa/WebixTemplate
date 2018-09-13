dbe83a5d0258943f05e181c53dc3defd735f8b8a
GlebNaz <nazemnovgleb@gmail.com>
2018-09-13 16:57:15 +0400

    added detailed part

--- data.js
+++ data.js
@@ -22,13 +22,5 @@ records_select = [
 datatable = [
     { id: "1", number_data: "32", type_vs_data: "BlindJet 543", preview_data: "fly_boeing", techstate_data: "A-check 23.08.18", cruiserSpeed_data: "850", maxWeightCapacity_data: "47000", maxFlightHeight_data: "2800", distance_data: "2800", fuelState_data: "96", airCompanyOwner_data: "FlyEmirates" },
     { id: "2", number_data: "54", type_vs_data: "Boeing 773", preview_data: "fly_boeing1", techstate_data: "C-check 21.08.18", cruiserSpeed_data: "900", maxWeightCapacity_data: "240000", maxFlightHeight_data: "11000", distance_data: "4350", fuelState_data: "100", airCompanyOwner_data: "Air Transit" },
-    { id: "", number_data: "", type_vs_data: " ", preview_data: "blank", techstate_data: "", cruiserSpeed_data: "", maxWeightCapacity_data: "", maxFlightHeight_data: "", distance_data: "", fuelState_data: "", airCompanyOwner_data: "" },
-    { id: "", number_data: "", type_vs_data: " ", preview_data: "blank", techstate_data: "", cruiserSpeed_data: "", maxWeightCapacity_data: "", maxFlightHeight_data: "", distance_data: "", fuelState_data: "", airCompanyOwner_data: "" },
-    { id: "", number_data: "", type_vs_data: " ", preview_data: "blank", techstate_data: "", cruiserSpeed_data: "", maxWeightCapacity_data: "", maxFlightHeight_data: "", distance_data: "", fuelState_data: "", airCompanyOwner_data: "" },
-    { id: "", number_data: "", type_vs_data: " ", preview_data: "blank", techstate_data: "", cruiserSpeed_data: "", maxWeightCapacity_data: "", maxFlightHeight_data: "", distance_data: "", fuelState_data: "", airCompanyOwner_data: "" },
-    { id: "", number_data: "", type_vs_data: " ", preview_data: "blank", techstate_data: "", cruiserSpeed_data: "", maxWeightCapacity_data: "", maxFlightHeight_data: "", distance_data: "", fuelState_data: "", airCompanyOwner_data: "" },
-    { id: "", number_data: "", type_vs_data: " ", preview_data: "blank", techstate_data: "", cruiserSpeed_data: "", maxWeightCapacity_data: "", maxFlightHeight_data: "", distance_data: "", fuelState_data: "", airCompanyOwner_data: "" },
-    { id: "", number_data: "", type_vs_data: " ", preview_data: "blank", techstate_data: "", cruiserSpeed_data: "", maxWeightCapacity_data: "", maxFlightHeight_data: "", distance_data: "", fuelState_data: "", airCompanyOwner_data: "" },
-    { id: "", number_data: "", type_vs_data: " ", preview_data: "blank", techstate_data: "", cruiserSpeed_data: "", maxWeightCapacity_data: "", maxFlightHeight_data: "", distance_data: "", fuelState_data: "", airCompanyOwner_data: "" },
-    { id: "", number_data: "", type_vs_data: " ", preview_data: "blank", techstate_data: "", cruiserSpeed_data: "", maxWeightCapacity_data: "", maxFlightHeight_data: "", distance_data: "", fuelState_data: "", airCompanyOwner_data: "" },
+
 ];
